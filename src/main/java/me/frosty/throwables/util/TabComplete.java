146d2b5be0a52cf1a8ccce9faa69c1672a262cfd
Frosty <frostyprime13@gmail.com>
2020-03-15 08:21:16 +0100

    Fix something

--- src/main/java/me/frosty/throwables/util/TabComplete.java
+++ src/main/java/me/frosty/throwables/util/TabComplete.java
@@ -4,6 +4,7 @@ import me.frosty.throwables.Throwables;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 
 import java.lang.reflect.Method;
@@ -15,59 +16,59 @@ import java.util.Map;
 public class TabComplete implements TabCompleter
 {
 
-	private final Throwables plugin;
+    private final Throwables plugin;
 
-	public TabComplete(final Throwables plugin)
-	{
-		this.plugin = plugin;
-	}
+    public TabComplete(final Throwables plugin)
+    {
+        this.plugin = plugin;
+    }
 
-	@Override
-	public List<String> onTabComplete(CommandSender s, Command cmd, String alias, String[] args)
-	{
-		if (cmd.getName().equals("throwables"))
-		{
-			if (args.length > 3)
-			{
-				return Collections.singletonList("");
-			}
+    @Override
+    public List<String> onTabComplete(CommandSender s, Command cmd, String alias, String[] args)
+    {
+        if (cmd.getName().equals("throwables"))
+        {
+            if (args.length > 3)
+            {
+                return Collections.emptyList();
+            }
 
-			if (args.length == 1)
-			{
-				return new ArrayList<String>()
-				{{
-					for (Map.Entry<String, Method> commandPair : plugin.getCommandsManager().getCommandManager().getCommands().entrySet())
-					{
-						if (commandPair.getKey().toLowerCase().startsWith(args[0].toLowerCase()))
-						{
-							add(commandPair.getKey());
-						}
-					}
-				}};
-			}
-			else if (args.length == 2)
-			{
-				List<String> types = new ArrayList<>();
-				for (String throwable : plugin.getConfig().getConfigurationSection("throwables").getKeys(false))
-				{
-					types.add(throwable);
-				}
+            if (args.length == 1)
+            {
+                final List<String> commands = new ArrayList<>();
+                for (final Map.Entry<String, Method> commandPair : plugin.getCommandsManager().getCommandManager().getCommands().entrySet())
+                {
+                    if (commandPair.getKey().toLowerCase().startsWith(args[0].toLowerCase()))
+                    {
+                        commands.add(commandPair.getKey());
+                    }
+                }
 
-				return types;
-			}
-			else if (args.length == 3)
-			{
-				List<String> players = new ArrayList<>();
-				for (Player plr : plugin.getServer().getOnlinePlayers())
-				{
-					players.add(plr.getName());
-				}
+                return commands;
+            }
+            else if (args.length == 2)
+            {
+                ConfigurationSection section = plugin.getConfig().getConfigurationSection("throwables");
+                if (section == null)
+                {
+                    return Collections.emptyList();
+                }
 
-				return players;
-			}
+                return new ArrayList<>(section.getKeys(false));
+            }
+            else if (args.length == 3)
+            {
+                final List<String> players = new ArrayList<>();
+                for (Player plr : plugin.getServer().getOnlinePlayers())
+                {
+                    players.add(plr.getName());
+                }
 
-		}
-		return Collections.emptyList();
-	}
+                return players;
+            }
+
+        }
+        return Collections.emptyList();
+    }
 
 }
